d21b482067e9e36ee4dc6e9da5327ca7ed22b0f4
Mayur Shah <mayur@kagrana.com>
2014-08-13 16:12:10 +0530

    Committing first project

---
+++ src/main/java/com/qamate/orangehrm_selenium_automation/App.java
@@ -0,0 +1,13 @@
+package com.qamate.orangehrm_selenium_automation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
