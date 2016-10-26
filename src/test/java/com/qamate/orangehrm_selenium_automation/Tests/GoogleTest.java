da2fe6c7a070b8ad5d2552b6f797e3eed0aa1fa3
msci <mayurb@softcrylic.co.in>
2016-10-26 07:04:07 +0530

    added testCase steps

--- src/test/java/com/qamate/orangehrm_selenium_automation/Tests/GoogleTest.java
+++ src/test/java/com/qamate/orangehrm_selenium_automation/Tests/GoogleTest.java
@@ -14,9 +14,10 @@ import java.util.List;
 public class GoogleTest extends BaseActions {
     @Test
     public void searchTest() throws InterruptedException {
-        driver.get("http://www.google.com");
+        this.testCase.addTestSteps("Home page opens.","User should see google.com","User can see google.com",true);
         WebElement element = driver.findElement(By.name("q"));
         element.sendKeys("Cheese!\n"); // send also a "\n"
+        this.testCase.addTestSteps("Search result page verification","User should see search result page","User can see search result",true);
         element.submit();
 
         WebElement myDynamicElement = (new WebDriverWait(driver, 10))
@@ -29,5 +30,7 @@ public class GoogleTest extends BaseActions {
             System.out.println(webElement.getAttribute("href"));
             Thread.sleep(1000);
         }
+        this.testCase.addTestSteps("Search result verification","All search result should be printed on console","Search result was printed on the console.",true);
+
     }
 }
